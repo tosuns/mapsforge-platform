328fc1b50837d020a779377ece25ebbb14595724
Serdar <Serdar@Yakumo>
2013-03-09 15:39:50 +0100

    add3

--- MapsforgePlatform/MapsForgeProjectDetectorModule/src/main/java/de/fub/mapsforge/project/detector/model/inference/InferenceModelResultDataSet.java
+++ MapsforgePlatform/MapsForgeProjectDetectorModule/src/main/java/de/fub/mapsforge/project/detector/model/inference/InferenceModelResultDataSet.java
@@ -4,9 +4,61 @@
  */
 package de.fub.mapsforge.project.detector.model.inference;
 
+import de.fub.agg2graph.structs.GPSTrack;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author Serdar
  */
 public class InferenceModelResultDataSet {
+
+    private final Map<String, GPSTrack> gpsTrackMap = new HashMap<String, GPSTrack>();
+
+    public int size() {
+        return gpsTrackMap.size();
+    }
+
+    public boolean isEmpty() {
+        return gpsTrackMap.isEmpty();
+    }
+
+    public boolean containsKey(Object key) {
+        return gpsTrackMap.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        return gpsTrackMap.containsValue(value);
+    }
+
+    public GPSTrack get(Object key) {
+        return gpsTrackMap.get(key);
+    }
+
+    public GPSTrack put(String key, GPSTrack value) {
+        return gpsTrackMap.put(key, value);
+    }
+
+    public GPSTrack remove(Object key) {
+        return gpsTrackMap.remove(key);
+    }
+
+    public void clear() {
+        gpsTrackMap.clear();
+    }
+
+    public Set<String> keySet() {
+        return gpsTrackMap.keySet();
+    }
+
+    public Collection<GPSTrack> values() {
+        return gpsTrackMap.values();
+    }
+
+    public Set<Map.Entry<String, GPSTrack>> entrySet() {
+        return gpsTrackMap.entrySet();
+    }
 }
