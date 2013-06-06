454b640fad4a48be44bc7929bab1199a3d1e827a
tosuns <tosun.serdar@googlemail.com>
2013-06-06 11:27:53 +0000

    + improvement to maps evaluation panel with an overview chart + improvement to the point-to-point map matcher + several other improvements and code factorings

---
+++ MapsforgePlatform/MapViewer/src/main/java/de/fub/mapviewer/tilesources/MapQuestOsmTileSource.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.fub.mapviewer.tilesources;
+
+import org.openide.util.lookup.ServiceProvider;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
+
+/**
+ *
+ * @author Serdar
+ */
+@ServiceProvider(service = TileSource.class)
+public class MapQuestOsmTileSource extends org.openstreetmap.gui.jmapviewer.tilesources.MapQuestOsmTileSource {
+}
